01cb592c829327fd1017475120e8f732cc173445
ruhuseyn <huseyinovrufet49@gmail.com>
2023-02-26 00:14:09 +0400

    Some Changes

--- src/main/java/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/microservices/limitsservice/controller/LimitsController.java
@@ -1,16 +1,22 @@
 package microservices.limitsservice.controller;
 
 import microservices.limitsservice.bean.Limits;
+import microservices.limitsservice.configuration.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ObjectInputFilter;
 import java.util.List;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private LimitsConfiguration limitsConfiguration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,1000);
+        return new Limits(limitsConfiguration.getMinimum(),limitsConfiguration.getMaximum());
     }
 }
