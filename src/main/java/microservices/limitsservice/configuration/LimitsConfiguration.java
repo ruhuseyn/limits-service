98e95de5a3155f9cf638f6b7acd3ccae689fd1e7
ruhuseyn <huseyinovrufet49@gmail.com>
2023-02-28 23:55:52 +0400

    Some Changes

---
+++ src/main/java/microservices/limitsservice/configuration/LimitsConfiguration.java
@@ -0,0 +1,14 @@
+package microservices.limitsservice.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Data
+public class LimitsConfiguration {
+
+    private int minimum;
+    private int maximum;
+}
